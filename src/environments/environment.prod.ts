35e15a0a3d117528b859aef5338b8c9253d39e36
Daniel Suarez <dan.suarez@outlook.com>
2021-01-04 14:01:02 -0300

    configurado para deploy en firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyEQG3SZDr9xddnnlRZgynDtl1L1_N0m0",
+    authDomain: "mastergym-srz.firebaseapp.com",
+    projectId: "mastergym-srz",
+    storageBucket: "mastergym-srz.appspot.com",
+    messagingSenderId: "558530200678",
+    appId: "1:558530200678:web:3947b1d8d4ef533601d9a7"
+  }
 };
